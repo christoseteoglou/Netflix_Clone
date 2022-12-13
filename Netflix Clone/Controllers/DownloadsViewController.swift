d638e792c7ae59c836c3a69218d49bc038c83ddf
eatoldglue <70583561+christoseteoglou@users.noreply.github.com>
2022-12-13 14:49:48 +0100

    first commit

---
+++ Netflix Clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadsViewController.swift
+//  Netflix Clone
+//
+//  Created by Christos Eteoglou on 2022-12-13.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Christos Eteoglou on 2022-12-13.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
